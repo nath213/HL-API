f11a24daa26cd16193801ae147925d3262415fae
nath213 <20233TN203@utez.edu.mx>
2025-05-10 21:01:23 -0600

    Agregando carpeta inicial
    Se subió la carpeta inicial del proyecto vacía

---
+++ HL_API/src/main/java/utez/edu/mx/hl_api/HlApiApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.hl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlApiApplication.class, args);
+	}
+
+}
---
+++ HL_API/src/test/java/utez/edu/mx/hl_api/HlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.hl_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
